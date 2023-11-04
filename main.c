366185360404f8f8335d7f1090fad83330ee1f81
reruo321 <48712088+reruo321@users.noreply.github.com>
2023-11-04 11:47:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,15 +10,15 @@ MODULE_AUTHOR("reruo");
 MODULE_DESCRIPTION("My First Simple Linux Device Driver - Virtual Keyboard");
 
 // Recognize and interpret a charp type value for the device_type module parameter.
-// device_type=charp_type_argument
+// device_type=your_charp_type_argument
 // (charp: char *. Defined by the Linux kernel's module infrastructure)
 module_param(device_type, charp, S_IRUGO);
 // Description of the Module Parameter
 MODULE_PARM_DESC(device_type, "My Virtual Keyboard");
 
 static int __init my_virtual_device_init(){
+        // check if a keyboard
         if(strcmp(device_type, "keyboard") == 0){
-
         }
         else{
                 pr_err("ERROR! Unknown device type: %s", device_type);
