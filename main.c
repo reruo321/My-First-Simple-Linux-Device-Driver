4e92b3c3aaff2c452290f8212c4c085fdb099460
reruo321 <48712088+reruo321@users.noreply.github.com>
2023-11-10 16:45:27 +0900

    added a guideline

--- main.c
+++ main.c
@@ -3,35 +3,32 @@
 
 static char *device_type = "keyboard";
 
-// Information about the Module
-// Use tools like lsmod and modinfo to see module information.
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("reruo");
 MODULE_DESCRIPTION("My First Simple Linux Device Driver - Virtual Keyboard");
 
-// module_param(name, type, perm)
-// typesafe helper for a module/cmdline parameter.
 module_param(device_type, charp, S_IRUGO);
-// Description of the Module Parameter
 MODULE_PARM_DESC(device_type, "My Virtual Keyboard");
 
-// Initialization function
+// Step 1: Module Initialization
 static int __init my_virtual_device_init(){
-        // check if a keyboard
-        if(strcmp(device_type, "keyboard") == 0){
-        }
-        else{
+        if(strcmp(device_type, "keyboard") != 0){
                 pr_err("ERROR! Unknown device type: %s", device_type);
                 return -EINVAL;
         }
 
+        // Perform device initialization here
+        // Register the device, perform memory allocations, etc.
+
         return 0;
 }
 
-// Cleanup and exit function
+module_init(my_virtual_device_init);
+
+// Step 16: Cleanup and Exit
 static void __exit my_virtual_device_exit(void) {
-    
+
 }
 
-module_init(my_virtual_device_init);
 module_exit(my_virtual_device_exit);
+
